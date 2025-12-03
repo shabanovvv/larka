26ce3d22e33d6712379624dc13f4b7ea66f0f23a
Vyacheslav Shabanov <shabanovvv@hopper-it.ru>
2025-12-03 14:05:54 +0300

    hw06 - добавил Breeze для авторизации входа в админ интерфейс

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,24 @@ use App\Http\Controllers\Admin\MentorProfileController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\TechnologyController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/admin')->name('admin.')->group(function () {
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('/admin')->middleware('auth')->name('admin.')->group(function () {
     Route::get('/', DashboardController::class)->name('dashboard');
 
     Route::resource('/technologies', TechnologyController::class);
@@ -15,3 +30,5 @@ Route::prefix('/admin')->name('admin.')->group(function () {
     Route::resource('/roles', RoleController::class);
     Route::resource('/mentor-profile', MentorProfileController::class);
 });
+
+require __DIR__.'/auth.php';
